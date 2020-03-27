2bff65965282b98e15a4f2ebcf45d0f1baf58a22
Krysta Wolkowiecki <krystawolkowiecki@outlook.com>
2020-03-27 07:08:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,12 @@
 def using_push(array, string)
   array.push(string)
 end
+
+def using_unshift(array,string)
+  array.shift(string)
+end
+
+def using_pop(array)
+  last= array.pop
+  return last
+end
